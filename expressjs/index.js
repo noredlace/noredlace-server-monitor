e7060b8668f86948d1435bf90f2a5a67eb7e2f2d
EJ Calderon <ejcalderon@gmail.com>
2021-11-18 02:09:02 -0600

    change .env path and URLs in .env

--- expressjs/index.js
+++ expressjs/index.js
@@ -3,7 +3,7 @@ const app = express();
 const fetch = require("node-fetch");
 require('dotenv').config();
 
-
+/*
 const MongoClient = require('mongodb').MongoClient;
 const uri = ""+process.env.API_MONGO_URI+"";
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -13,11 +13,24 @@ client.connect(err => {
   console.log('Connected!!!');
   client.close();
 });
+*/
 
+console.log(process.env);
 
+const url_minecraft = "https://api.mcsrvstat.us/2/minecraft.noredlace.com"
+const url_terraria = "https://terraria-servers.com/api/?object=servers&element=detail&key="
+const url_sdtd = "https://7daystodie-servers.com/api/?object=servers&element=detail&key="
+const url_dst = "https://s3.amazonaws.com/klei-lobby/US-Steam-noevent.json.gz"
+const url_empyrion = "https://empyrion-servers.com/api/?object=servers&element=detail&key="
+const url_theforest = "https://empyrion-servers.com/api/?object=servers&element=detail&key="
+const url_valheim = "https://valheim-servers.io/api/?object=servers&element=detail&key="
+
+const api_key_dst = process.env.API_KEY_DST
 const api_key_terraria = process.env.API_KEY_TERRARIA
 const api_key_sdtd = process.env.API_KEY_SDTD
-const api_key_dst = process.env.API_KEY_DST
+const api_key_empyrion = process.env.API_KEY_EMPYRION
+const api_key_theforest = process.env.API_KEY_THEFOREST
+const api_key_valheim = process.env.API_KEY_VALHEIM || "qVyyfChBMd5IpGSGNyE97pQf2sX4Bf8qQd"
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -26,18 +39,18 @@ app.use(function(req, res, next) {
 });
 
 app.get('/api/servers', (req, res) => {
-  res.send(['Minecraft', 'Terraria', 'Dont Starve Together', '7 Days To Die', 'Empyrion Server']);
+  res.send(['Minecraft', 'Terraria', 'Dont Starve Together', '7 Days To Die', 'Empyrion Server', 'Valheim Server', 'The Forest']);
 });
 
 app.get('/api/minecraft', async (req, res) => {
-  const api_url = 'https://api.mcsrvstat.us/2/noredlace.us.to:25565';
+  const api_url = url_minecraft;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
   res.json(json);
 });
 
 app.get('/api/terraria', async (req, res) => {
-  const api_url = 'https://terraria-servers.com/api/?object=servers&element=detail&key='+api_key_terraria+'';
+  const api_url = url_terraria + api_key_terraria;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
   res.json(json);
@@ -45,7 +58,7 @@ app.get('/api/terraria', async (req, res) => {
 
 
 app.get('/api/dst', async (req, res) => {
-  const api_url = 'https://s3.amazonaws.com/klei-lobby/US-Steam-noevent.json.gz';
+  const api_url = url_dst;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
   
@@ -119,11 +132,33 @@ app.get('/api/dst', async (req, res) => {
 
 
 app.get('/api/sdtd', async (req, res) => {
-  const api_url = 'https://7daystodie-servers.com/api/?object=servers&element=detail&key='+api_key_sdtd+'';
+  const api_url = url_sdtd + api_key_sdtd;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
   res.json(json);
 });
+
+app.get('/api/empyrion', async (req, res) => {
+	const api_url = url_empyrion + api_key_empyrion;
+	const fetch_response = await fetch(api_url);
+	const json = await fetch_response.json();
+	res.json(json);
+  });
+
+app.get('/api/theforest', async (req, res) => {
+	const api_url = url_theforest + api_key_theforest;
+	const fetch_response = await fetch(api_url);
+	const json = await fetch_response.json();
+	res.json(json);
+  });
+
+
+app.get('/api/valheim', async (req, res) => {
+	const api_url = url_valheim + api_key_valheim;
+	const fetch_response = await fetch(api_url);
+	const json = await fetch_response.json();
+	res.json(json);
+  });
 
 app.get('/api/jokes/jod', async (req, res) => {
   const api_url = 'https://api.jokes.one/jod';
