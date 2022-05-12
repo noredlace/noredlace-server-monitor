88990ef186f56e95d1f009d2b6aada78d70abd99
EJ Calderon <ejcalderon@gmail.com>
2022-05-12 10:55:16 -0500

    change to promise all to reduce waittime

--- expressjs/index.js
+++ expressjs/index.js
@@ -15,7 +15,7 @@ const serverUser = config.ServerCreds.UserName
 const serverPass = config.ServerCreds.Password
 
 //Function to support SSH commands to Server
-function executeSSHCommand(_serverHost, _serverUser, _serverPass, _serverCommand) {
+function executeSSHCommand(_os, _serverHost, _serverUser, _serverPass, _serverCommand, _game, _name, _savedfileurl, _address, _port) {
 	try {
 		return new Promise((resolve, reject) => {
 			exec(_serverCommand, {
@@ -32,7 +32,70 @@ function executeSSHCommand(_serverHost, _serverUser, _serverPass, _serverCommand
 				//return each line to an array
 				result = result.split('\n');
 
-				resolve(result)
+				//reinitializare the json result
+				var jsonResult = {
+					"Game": _game,
+					"Name": _name,
+					"SavedFileURL": _savedfileurl,
+					"Address": _address,
+					"Port": _port,
+					"Description": _game,
+					"IsOnline": false,
+					"Version": "N/A",
+				}
+
+				if (_os == 'Windows'){
+					if (result == -1) {
+						return;
+					}
+		
+					jsonResult = {
+						"Game": _game,
+						"Name": _name,
+						"SavedFileURL": _savedfileurl,
+						"Address": _address,
+						"Port": _port,
+						"Description": _game,
+						"IsOnline": result.length > 2 ? true : false,
+						"Version": "N/A",
+					}
+				}
+				else if (_os == 'Linux'){
+					if (result == -1) {
+						return;
+					}
+		
+					result.forEach(function (currentValue, index, arr) {
+						if (/Internet IP/g.test(currentValue)) {
+							serverIPString = currentValue
+						}
+						if (/Status/g.test(currentValue)) {
+							serverStatusString = currentValue
+						}
+						//Appears in LinuxGSM, Satisfactory uses the Server IP and not the Internet IP
+						if (_game == 'Satisfactory') {
+							if (/Server IP/g.test(currentValue)) {
+								serverIPString = currentValue
+							}
+						}
+					});
+		
+					var serverIPArray = serverIPString.split(':');
+					var serverStatusArray = serverStatusString.split(':');
+		
+					jsonResult = {
+						"Game": _game,
+						"Name": _name,
+						"SavedFileURL": _savedfileurl,
+						"Address": serverIPArray.length > 1 ? serverIPArray[1].trim() : "N/A",
+						"Port": serverIPArray.length > 2 ? serverIPArray[2].trim() : "N/A",
+						"Description": _game,
+						"IsOnline": serverStatusArray[1].trim() == 'STARTED' ? true : false,
+						"Version": "N/A",
+					}
+				}
+
+				resolve(jsonResult);
 			})
 		})
 	}
@@ -47,8 +110,9 @@ app.use(function (req, res, next) {
 	next();
 });
 
-app.get('/api/gameservers', async (req, res) => {
-	var result = '[';
+app.get('/api/gameservers', async (req,res) => {
+	var promiseArray = [];
+
 	for (var i = 0; i < config.Games.length; i++) {
 		//Grab the relevant Info off the Config Json for the Game
 		const os = config.Games[i].OS;
@@ -63,101 +127,19 @@ app.get('/api/gameservers', async (req, res) => {
 		const isonline = config.Games[i].IsOnline;
 		const version = config.Games[i].Version;
 
-		var results;
-		var serverIPString;
-		var serverStatusString;
-
-		//Reset the Json Result on Loop
-		var jsonResult = {
-			"Game": game,
-			"Name": name,
-			"SavedFileURL": savedfileurl,
-			"Address": address,
-			"Port": port,
-			"Description": game,
-			"IsOnline": false,
-			"Version": "N/A",
-		}
-
-		//console.log("Game: " + game + ", Index: " + i + ", Length: " + config.Games.length);
-
-		/*
-		Run the SSH Command on Windows for the LinuxGSM Details Page
-		Grab the ServerIP and ServerStatus by parsing the STDOut Page
-		*/
-		if (os == 'Linux') {
-			results = await executeSSHCommand(serverhost, serverUser, serverPass, servercommand);
-			if (results == -1) {
-				continue;
-			}
-
-			results.forEach(function (currentValue, index, arr) {
-				if (/Internet IP/g.test(currentValue)) {
-					serverIPString = currentValue
-				}
-				if (/Status/g.test(currentValue)) {
-					serverStatusString = currentValue
-				}
-				//Appears in LinuxGSM, Satisfactory uses the Server IP and not the Internet IP
-				if (game == 'Satisfactory') {
-					if (/Server IP/g.test(currentValue)) {
-						serverIPString = currentValue
-					}
-				}
-			});
-
-			var serverIPArray = serverIPString.split(':');
-			var serverStatusArray = serverStatusString.split(':');
-
-			jsonResult = {
-				"Game": game,
-				"Name": name,
-				"SavedFileURL": savedfileurl,
-				"Address": serverIPArray.length > 1 ? serverIPArray[1].trim() : "N/A",
-				"Port": serverIPArray.length > 2 ? serverIPArray[2].trim() : "N/A",
-				"Description": game,
-				"IsOnline": serverStatusArray[1].trim() == 'STARTED' ? true : false,
-				"Version": "N/A",
-			}
-		}
-
-
-		/*
-		Run the SSH Command on Windows for the .exe process
-		Should be a list of current running Tasks/Processes with a matching Name
-		If we have more than 2 results, then our process is running
-		*/
-		else if (os == 'Windows') {
-			results = await executeSSHCommand(serverhost, serverUser, serverPass, servercommand);
-
-			if (results == -1) {
-				continue;
-			}
-
-			jsonResult = {
-				"Game": game,
-				"Name": name,
-				"SavedFileURL": savedfileurl,
-				"Address": address,
-				"Port": port,
-				"Description": game,
-				"IsOnline": results.length > 2 ? true : false,
-				"Version": "N/A",
-			}
-		}
-
-		//Add Stringified Json Object from Above Steps 
-		result += JSON.stringify(jsonResult);
-
-		//Add Comma for an Array of JSON objects. Only if this isn't the last element
-		if (i != config.Games.length - 1) {
-			result += ',';
-		}
-
+		promiseArray.push(executeSSHCommand(os, serverhost, serverUser, serverPass, servercommand, game, name, savedfileurl, address, port))
 	}
-	result += ']';
-	//res.json(jsonResults);
-	res.json(JSON.parse(result));
+
+	Promise.all(promiseArray).then((result) => {
+		//console.log(result);
+		res.json(result);
+
+	})
+	.catch((e) => {
+		//console.log("Errors: " + e);
+		res.json('{"Error": -1}');
+	})
+
 
 });
 
